dd1111f2e0685d1689d5f6e13744dd57e5d48781
mosuo1913 <44077303+mosuo1913@users.noreply.github.com>
2021-09-20 16:02:23 +0900

    mapやfilterの学習

--- src/index.js
+++ src/index.js
@@ -142,3 +142,33 @@
 // const arr8 = [...arr4];
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapややfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "佐藤"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "佐藤") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
