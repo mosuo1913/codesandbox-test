a5081f8c5f6e022fe63d9dd4f197d4e1b6efaec2
mosuo1913 <44077303+mosuo1913@users.noreply.github.com>
2021-09-20 14:32:03 +0900

    const, letの学習

---
+++ src/index.js
@@ -0,0 +1,48 @@
+/**
+ * const, let等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // varは上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // varは再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// constは上書き不可
+// val3 = "const変数を上書き";
+
+// constは再宣言不可能
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "もすお",
+//   age: 24
+// };
+// val4.name = "けっけ";
+// val4.age = 22;
+// val4.address = "北海道";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
