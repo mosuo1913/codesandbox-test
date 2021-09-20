ddd7b31cb87b65a31686b99f3f425545950959e1
mosuo1913 <44077303+mosuo1913@users.noreply.github.com>
2021-09-20 16:12:14 +0900

    論理演算子の本当の意味に関する学習

--- src/index.js
+++ src/index.js
@@ -191,3 +191,26 @@
 //   return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です。";
 // };
 // console.log(checkSum(50, 50));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueです");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueです");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
