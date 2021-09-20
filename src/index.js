3ae21f18801d4adc37c6300f25aa0df76b0562d7
mosuo1913 <44077303+mosuo1913@users.noreply.github.com>
2021-09-20 14:35:09 +0900

    テンプレート文字列の学習

--- src/index.js
+++ src/index.js
@@ -46,3 +46,18 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "もすお";
+// const age = 24;
+// 「私の名前はもすおです。年齢は24歳です。」
+
+// 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
